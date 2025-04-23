bd10754d0ca19686bcfba06c6355299d7fa1f805
bmermino <boris.merminod@edu.univ-fcomte.fr>
2025-04-23 21:31:54 +0200

    Tuto 3 : Exemple d'api réalisé avec ASP.net

---
+++ TodoAPI/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
+
+namespace TodoAPI.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+}
